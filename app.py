592f15c5a01d83ff36810d4fb78e90e97a84cc29
NathanFarinha10 <nathan.farinha10@gmail.com>
2025-07-08 17:38:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from streamlit_gsheets import GSheetsConnection
+from st_gsheets_connection import GSheetsConnection
 
 # =================================================================================
 # CONFIGURAÇÕES DA PÁGINA
